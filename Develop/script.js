8335d82ce471479a3b8dc6397206fa7462a90111
EmanSL <emanuelsilvalopez@gmail.com>
2022-01-10 00:22:30 -0500

    added message that will prompt user to use at least one criteria

--- Develop/script.js
+++ Develop/script.js
@@ -5,17 +5,21 @@ function generatePassword() {
   //    a. password length 8 < 128
   if (promptLength < 8 || promptLength > 128 ) {
     window.alert("You did not enter a valid number, please try again.");
+    generatePassword();
+  } else {
+    var uppercaseConfirm = window.confirm('Would you like your password to contain uppercase letters? If yes click OK.');
 
-    return generatePassword();
+    var lowercaseConfirm = window.confirm('Would you like your password to contain lowercase letters? If yes click OK.');
+
+    var numberConfirm = window.confirm('Would you like your password to contain numbers? If yes click OK.');
+
+    var symbolConfirm = window.confirm('Would you like your password to contain symbols? If yes click OK.');
+  } if (uppercaseConfirm === false && lowercaseConfirm === false && numberConfirm === false && symbolConfirm === false) {
+    window.alert('You must choose a criteria!');
+    generatePassword();
   }
 
-  var uppercaseConfirm = window.confirm('Would you like your password to contain uppercase letters? If yes click OK.');
-
-  var lowercaseConfirm = window.confirm('Would you like your password to contain lowercase letters? If yes click OK.');
-
-  var numberConfirm = window.confirm('Would you like your password to contain numbers? If yes click OK.');
-
-  var symbolConfirm = window.confirm('Would you like your password to contain symbols? If yes click OK.');
+  return getRandomLower()
 }
   //    b. Lowercase, Uppercase, Number, Symbol
 
