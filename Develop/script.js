14de5f5a98b0058efd44ef9701d3bdd11daf7e67
EmanSL <emanuelsilvalopez@gmail.com>
2022-01-10 15:33:03 -0500

    removed recursive bug in characterConfirm(); also fixed scope issues caused by chosenChars & userPassword

--- Develop/script.js
+++ Develop/script.js
@@ -1,66 +1,72 @@
-// Assignment code here
-
-var chosenChars = [];
-
-var userPassword = "";
 // Lower Characters Array
 const lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 // Upper Characters Array
 const upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 // Special characters Array
-const special = ['!','@','#','$','%','^','&','*','(',')','*','+',',','-','.','/',';',':','<','=','>','?','[',']','_','`','{','}','|','`'];
+const special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '*', '+', ',', '-', '.', '/', ';', ':', '<', '=', '>', '?', '[', ']', '_', '`', '{', '}', '|', '`'];
 // numbers array
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-
 function generatePassword() {
+
   var promptLength = parseInt(window.prompt('How many characters would you like your password to contain? Please choose between 8 - 128.'));
 
   var regEx = /^\d*$/;
 
-  if (!regEx.test(promptLength)) {
-    alert("this is not a number!")
-    generatePassword();
-  }
+  var chosenChars = [];
 
-  if (promptLength < 8 || promptLength > 128 || promptLength === NaN) {
-    window.alert("You did not enter a valid number, please try again.");
-    generatePassword();
+  var userPassword = "";
+
+  function characterConfirm() {
+    
+    var uppercaseConfirm = false;
+    var lowercaseConfirm = false;
+    var numberConfirm = false;
+    var symbolConfirm = false;
+
+    uppercaseConfirm = window.confirm('Would you like your password to contain uppercase letters? If yes click OK.');
+
+    lowercaseConfirm = window.confirm('Would you like your password to contain lowercase letters? If yes click OK.');
+
+    numberConfirm = window.confirm('Would you like your password to contain numbers? If yes click OK.');
+
+    symbolConfirm = window.confirm('Would you like your password to contain symbols? If yes click OK.');
+
+    if (uppercaseConfirm === false && lowercaseConfirm === false && numberConfirm === false && symbolConfirm === false) {
+      window.alert('You must choose a criteria!');
+      generatePassword();
+    } else {
+
+      if (uppercaseConfirm === true) {
+        chosenChars = chosenChars.concat(upper);
+      }
+      if (lowercaseConfirm === true) {
+        chosenChars = chosenChars.concat(lower);
+      }
+      if (numberConfirm === true) {
+        chosenChars = chosenChars.concat(numbers);
+      }
+      if (symbolConfirm === true) {
+        chosenChars = chosenChars.concat(special);
+      }
+    }
   };
 
-  characterConfirm();
-
-  for (var i = 0; i < promptLength; i++) {
-    var randomCharacters = chosenChars[Math.floor(Math.random() * chosenChars.length)];
-    userPassword += randomCharacters;
-  }
-  return userPassword;
-};
-
-function characterConfirm() {
-  var uppercaseConfirm = window.confirm('Would you like your password to contain uppercase letters? If yes click OK.');
-
-    var lowercaseConfirm = window.confirm('Would you like your password to contain lowercase letters? If yes click OK.');
-
-    var numberConfirm = window.confirm('Would you like your password to contain numbers? If yes click OK.');
-
-    var symbolConfirm = window.confirm('Would you like your password to contain symbols? If yes click OK.');
-
-  if (uppercaseConfirm === false && lowercaseConfirm === false && numberConfirm === false && symbolConfirm === false) {
-    window.alert('You must choose a criteria!');
+  if (!regEx.test(promptLength)) {
+    alert("this is not a number!");
     generatePassword();
-  }
-  if (uppercaseConfirm === true) {
-    chosenChars = chosenChars.concat(lower);
-  }
-  if (lowercaseConfirm === true) {
-    chosenChars = chosenChars.concat(upper);
-  }
-  if (numberConfirm === true) {
-    chosenChars = chosenChars.concat(numbers);
-  }
-  if (symbolConfirm === true) {
-    chosenChars = chosenChars.concat(special);
+  } else if (promptLength < 8 || promptLength > 128 || promptLength === NaN) {
+    window.alert("You did not enter a valid number, please try again.");
+    generatePassword();
+  } else {
+
+    characterConfirm();
+
+    for (var i = 0; i < promptLength; i++) {
+      var randomCharacters = chosenChars[Math.floor(Math.random() * chosenChars.length)];
+      userPassword += randomCharacters;
+    }
+    return userPassword;
   }
 };
 
